800443dddee23af6de1e5b48b5e1269af7041811
jifengdong <58116847+jifengdong@users.noreply.github.com>
2021-09-12 10:56:06 +0800

    Update mxz.js

--- mxz.js
+++ mxz.js
@@ -1,7 +1,17 @@
 /*
 密小助解锁VIP，恋爱话语
-https:\/\/apic\.yzhiyin\.com\/index\.php\/v4\/new\/duihua\/list
+https://apic.yzhiyin.com/index.php/v4/new/vip/memberVip
 */
 
-body = $response.body.replace(/\"is_open\":\d/g, "\"is_open\":1")
-$done({body});
+let obj = JSON.parse($response.body);
+
+obj.data = {
+    "is_sign": 0,
+    "bind_num": 3,
+    "end_time": 4077190060,
+    "create_time": 1581871195,
+    "update_time": 1581871195,
+    "remark": "",
+    "free_search": 0
+}
+$done({body: JSON.stringify(obj)});
